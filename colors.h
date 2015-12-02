aaeaa4f781ad7d6661a2a0e0d808599433182821
Marie <marie.darrigol@e.ujf-grenoble.fr>
2015-12-02 18:29:51 -0500

    Préparation du fichier de définition des couleurs

---
+++ colors.h
@@ -0,0 +1,52 @@
+#ifndef COLORS
+#define COLORS
+
+/*
+ *
+ * Here are defined all the colors used in our application
+ *
+*/
+/*
+// --- Menu colors (used in the home page, the menu etc.)
+#define @menuLighterColor        0xe6e6e6
+#define @menuLightColor          0xa2a2a2
+#define @menuMediumColor         0x888888
+#define @menuDarkColor           0x5f5f5f
+#define @menuDarkerColor         0x353535
+#define @menuDarkestColor        0x191919
+
+// --- TV Series page colors
+#define tvSeriesLighterColor    0xd7daff
+#define tvSeriesLightColor      0x8f9aff
+#define tvSeriesMediumColor     0x233dbc
+#define tvSeriesDarkColor       0x14288a
+#define tvSeriesDarkerColor     0x071351
+#define tvSeriesDarkestColor	0x07103c
+
+// --- Anime page colors
+#define animesLighterColor		0xffe5e9
+#define animesLightColor		0xe87585
+#define animesMediumColor		0xb21328
+//#define animesDarkColor
+//#define animesDarkerColor
+//#define animesDarkestColor
+
+// --- Podcasts page colors
+#define podcastsLighterColor    0xdafdda
+#define podcastsLightColor      0x66cc66
+#define podcastsMediumColor		0x336633
+//#define podcastsDarkColor
+//#define podcastsDarkerColor
+//#define podcastsDarkestColor
+
+// --- Books page colors
+#define booksLighterColor       0xeddbff
+#define booksLightColor         0x9966cc
+#define booksMediumColor		0x330066
+//#define booksDarkColor
+//#define booksDarkerColor
+//#define booksDarkestColor
+*/
+
+#endif // COLORS
+

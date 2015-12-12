0d736c61172d228377fb3ab74e57ad2169339b6a
Marie <marie.darrigol@e.ujf-grenoble.fr>
2015-12-11 19:14:16 -0500

    Episode added dynamically in the list widget

--- pageonetvserie.cpp
+++ pageonetvserie.cpp
@@ -6,6 +6,10 @@ PageOneTVSerie::PageOneTVSerie(QWidget *parent) :
     ui(new Ui::PageOneTVSerie)
 {
     ui->setupUi(this);
+
+    // Creation of the list of episodes
+    QListWidget *listWidget = ui->listEpisodesWidget;
+    new QListWidgetItem(tr("Episode 1"), listWidget);
 }
 
 PageOneTVSerie::~PageOneTVSerie()
